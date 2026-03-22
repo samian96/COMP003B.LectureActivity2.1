07cb52413ccd24ee0f8f6691b97281fa4545f86b
samian <sam.iannazzo@yahoo.com>
2026-03-22 14:13:28 -0700

    registered middleware in RequestLogging

--- COMP003B.LectureActivity2.1/Middleware/RequestLoggingMiddleware.cs
+++ COMP003B.LectureActivity2.1/Middleware/RequestLoggingMiddleware.cs
@@ -2,5 +2,18 @@
 {
     public class RequestLoggingMiddleware
     {
+        private readonly RequestDelegate _next;
+
+        public RequestLoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            Console.WriteLine($"[Request] {context.Request.Method} {context.Request.Path}");
+            await _next(context);
+            Console.WriteLine($"[Response] {context.Response.StatusCode}");
+        }
     }
 }
